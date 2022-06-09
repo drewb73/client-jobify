f9779d0c0b25c73b9160850d4a71f1e1dd18c8ca
drewb73 <bartondrew5@gmail.com>
2022-06-09 09:01:07 -0700

    error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import img from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
 
 function Error() {
   return (
-    <h1>Error</h1>
+    <Wrapper className='full-page'>
+        <div>
+            <img src={img} alt='not found' />
+            <h3>text</h3>
+            <p>text</p>
+            <Link to='/'>Back Home</Link>
+        </div>
+    </Wrapper>
   )
 }
 
