d174d293fa7160f8495cfe4f28cef2b1ba6addad
drewb73 <bartondrew5@gmail.com>
2022-06-15 13:32:22 -0700

    added comments

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useAppContext } from '../context/appContext'
 
+//create error alert for login and register page
 const Alert = () => {
   const {alertType, alertText} = useAppContext()
   return (
