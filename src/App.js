f8b033c21bd138f55ad76922d6675e741ac172ed
drewb73 <bartondrew5@gmail.com>
2022-06-25 18:27:18 -0700

    dashboard pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {Dashboard, Register, Landing, Error} from './pages'
+import { Register, Landing, Error} from './pages'
 
 
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<div>dashboard</div>} />
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error/>} />
