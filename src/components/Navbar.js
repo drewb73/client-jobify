fba5a3f8107c54133777d7a117fbe3d903d247c1
drewb73 <bartondrew5@gmail.com>
2022-06-28 14:03:07 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
                 <FaCaretDown />
               </button>
               <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
-                <button type='button' className='dropdown-btn' onClick={() => logoutUser()}>
+                <button type='button' className='dropdown-btn' onClick={logoutUser}>
                     logout
                 </button>
               </div>
