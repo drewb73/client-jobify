cb625a442fefaf3facb91389685e89b2b17359a5
drewb73 <bartondrew5@gmail.com>
2022-06-28 14:40:53 -0700

    small sidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import {FaTimes} from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import links from '../utils/links'
+import { NavLink } from 'react-router-dom'
+import Logo from './Logo'
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-        <h4>small sidebar</h4>
+        <div className='sidebar-container show-sidebar'>
+            <div className='content'>
+                <button type='button' className='clost-btn' onClick={() => console.log('toggle')}>
+                    <FaTimes />
+                </button>
+                <header>
+                    <Logo />
+                </header>
+                <div className='nav-links'>nav links</div>
+            </div>
+        </div>
     </Wrapper>
   )
 }
