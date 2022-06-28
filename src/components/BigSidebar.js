6ea781b8bf8749c7d1de859be716dd5969fea072
drewb73 <bartondrew5@gmail.com>
2022-06-28 15:21:56 -0700

    Update BigSidebar.js

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/BigSidebar'
+import { useAppContext } from '../context/appContext'
+import NavlLinks from './NavlLinks'
+import Logo from './Logo'
+
 
 const BigSidebar = () => {
+const {showSidebar} = useAppContext()
   return (
     <Wrapper>
-        <h4>big sidebar</h4>
+        <div className={showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'}>
+            <div className='content'>
+                <header>
+                    <Logo />
+                </header>
+                <NavlLinks />
+            </div>
+        </div>
     </Wrapper>
   )
 }
