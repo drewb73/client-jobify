a350c77f63c74974f7fa8aefbcd55062ab9c3b4c
drewb73 <bartondrew5@gmail.com>
2022-06-27 18:07:17 -0700

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
+import {FaAlignLeft, FaUserCircle, FaCaretDown} from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
 
 const Navbar = () => {
   return (
     <Wrapper>
-        <h4>navbar</h4>
+      <div className='nav-center'>
+          <button className='toggle-btn' onClick={() => console.log('toggle sidebar')}>
+              <FaAlignLeft />
+          </button>
+
+          <div>
+              <Logo />
+              <h3 className='logo-text'>dashboard</h3>
+          </div>
+
+          <div className='btn-container'>
+              <button className='btn' onClick={() => console.log('show logout')}>
+                <FaUserCircle />
+                Drew
+                <FaCaretDown />
+              </button>
+              <div className='dropdown show-dropdown'>
+                <button className='dropdown-btn' onClick={() => console.log('logout user')}>
+                    logout
+                </button>
+              </div>
+          </div>
+      </div>
     </Wrapper>
   )
 }
