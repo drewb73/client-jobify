85f1d02a7d05104974ace0d4ec5bacb6486e5ba8
drewb73 <bartondrew5@gmail.com>
2022-06-30 12:21:38 -0700

    Update Profile.js

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -29,7 +29,7 @@ const Profile = () => {
         {/* name */}
         <div className='form-center'>
           <FormRow type='text' name='name' value={name} handleChange={(e) => setName(e.target.value)} />
-          <FormRow type='text' name='lastName' value={lastName} handleChange={(e) => setLastName(e.target.value)} />
+          <FormRow type='text' labelText='last name' name='lastName' value={lastName} handleChange={(e) => setLastName(e.target.value)} />
           <FormRow type='email' name='email' value={email} handleChange={(e) => setEmail(e.target.value)} />
           <FormRow type='text' name='location' value={location} handleChange={(e) => setLocation(e.target.value)} />
 
