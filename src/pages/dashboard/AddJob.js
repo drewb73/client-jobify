d44758930777e43a448a4c3659e23e92ddf61527
drewb73 <bartondrew5@gmail.com>
2022-07-05 09:54:38 -0700

    Update AddJob.js

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import {FormRow, Alert} from '../../components'
+import { useAppContext } from '../../context/appContext'
+import Wrapper from '../../assets/wrappers/DashboardFormPage'
+
+
 
 const AddJob = () => {
+  const {
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+  } = useAppContext()
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+
+  if (!position || !company || !jobLocation) {
+    displayAlert()
+    return
+  }
+}
+
   return (
     <h1>AddJob</h1>
   )
