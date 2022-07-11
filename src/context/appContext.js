35f895814c4833a9e5580f9fb2f547ea2a898d6d
drewb73 <bartondrew5@gmail.com>
2022-07-11 14:08:05 -0700

    Update appContext.js

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,7 +1,7 @@
 import React, { useReducer, useContext} from 'react'
 import reducer from './reducer'
 import axios from 'axios'
-import { DISPLAY_ALERT, CLEAR_ALERT, SETUP_USER_BEGIN, SETUP_USER_SUCCESS, SETUP_USER_ERROR, TOGGLE_SIDEBAR, LOGOUT_USER, UPDATE_USER_BEGIN, UPDATE_USER_SUCCESS, UPDATE_USER_ERROR, HANDLE_CHANGE, CLEAR_VALUES, CREATE_JOB_BEGIN, CREATE_JOB_SUCCESS, CREATE_JOB_ERROR, GET_JOBS_BEGIN, GET_JOBS_SUCCESS, SET_EDIT_JOB, DELETE_JOB_BEGIN } from "./actions"
+import { DISPLAY_ALERT, CLEAR_ALERT, SETUP_USER_BEGIN, SETUP_USER_SUCCESS, SETUP_USER_ERROR, TOGGLE_SIDEBAR, LOGOUT_USER, UPDATE_USER_BEGIN, UPDATE_USER_SUCCESS, UPDATE_USER_ERROR, HANDLE_CHANGE, CLEAR_VALUES, CREATE_JOB_BEGIN, CREATE_JOB_SUCCESS, CREATE_JOB_ERROR, GET_JOBS_BEGIN, GET_JOBS_SUCCESS, SET_EDIT_JOB, DELETE_JOB_BEGIN, EDIT_JOB_BEGIN, EDIT_JOB_SUCCESS, EDIT_JOB_ERROR } from "./actions"
 
 const token = localStorage.getItem('token')
 const user = localStorage.getItem('user')
@@ -194,8 +194,29 @@ const AppProvider = ({children}) => {
     const setEditJob = (id) => {
         dispatch({type: SET_EDIT_JOB, payload: {id}})
     }
-    const editJob = () => {
-        console.log('edit Job')
+    const editJob = async () => {
+        dispatch({ type: EDIT_JOB_BEGIN})
+        try {
+            const {position, company, jobLocation, jobType, status} = state
+            await authFetch.patch(`/jobs/${state.editJobId}`, {
+                company,
+                position,
+                jobLocation,
+                jobType,
+                status,
+            })
+            dispatch({
+                type: EDIT_JOB_SUCCESS,
+            })
+            dispatch({ type: CLEAR_VALUES})
+        } catch (error) {
+            if (error.response.status === 401) return
+            dispatch({
+                type: EDIT_JOB_ERROR,
+                payload: {msg: error.response.data.msg},
+            })
+        }
+        clearAlert()
     }
 
     const deleteJob = async (jobId) => {
