7c482d22c6b40db539aff653d9aaa8797b5ebf78
drewb73 <bartondrew5@gmail.com>
2022-07-11 14:10:48 -0700

    Update reducer.js

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,4 +1,4 @@
-import { DISPLAY_ALERT, CLEAR_ALERT, SETUP_USER_BEGIN, SETUP_USER_SUCCESS, SETUP_USER_ERROR, TOGGLE_SIDEBAR, LOGOUT_USER, UPDATE_USER_BEGIN, UPDATE_USER_SUCCESS, UPDATE_USER_ERROR, HANDLE_CHANGE, CLEAR_VALUES, CREATE_JOB_BEGIN, CREATE_JOB_SUCCESS, CREATE_JOB_ERROR, GET_JOBS_BEGIN, GET_JOBS_SUCCESS, SET_EDIT_JOB, DELETE_JOB_BEGIN } from "./actions"
+import { DISPLAY_ALERT, CLEAR_ALERT, SETUP_USER_BEGIN, SETUP_USER_SUCCESS, SETUP_USER_ERROR, TOGGLE_SIDEBAR, LOGOUT_USER, UPDATE_USER_BEGIN, UPDATE_USER_SUCCESS, UPDATE_USER_ERROR, HANDLE_CHANGE, CLEAR_VALUES, CREATE_JOB_BEGIN, CREATE_JOB_SUCCESS, CREATE_JOB_ERROR, GET_JOBS_BEGIN, GET_JOBS_SUCCESS, SET_EDIT_JOB, DELETE_JOB_BEGIN, EDIT_JOB_BEGIN, EDIT_JOB_SUCCESS, EDIT_JOB_ERROR } from "./actions"
 import { initialState } from "./appContext"
 
 
@@ -152,6 +152,27 @@ const reducer = (state, action) => {
     }
     if (action.type === DELETE_JOB_BEGIN) {
         return {...state, isLoading: true}
+    }
+    if(action.type === EDIT_JOB_BEGIN) {
+        return {...state, isLoading: true}
+    }
+    if (action.type === EDIT_JOB_SUCCESS) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: 'success',
+            alertText: 'Job Updated!',
+        }
+    }
+    if(action.type === EDIT_JOB_ERROR) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: 'danger',
+            alertText: action.payload.msg,
+        }
     }
     throw new Error(`no such action :${action.type}`)
 }
