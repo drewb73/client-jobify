2f62f6961b9f433def9ce8678bd0eaee8a1c284f
drewb73 <bartondrew5@gmail.com>
2022-07-15 11:33:58 -0700

    Update SearchContainer.js

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,6 +1,25 @@
 import React from 'react'
+import {FormRow, FormRowSelect} from '.'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/SearchContainer'
 
 const SearchContainer = () => {
+  const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext()
+
+
+
+
   return (
     <div>SearchContainer</div>
   )
